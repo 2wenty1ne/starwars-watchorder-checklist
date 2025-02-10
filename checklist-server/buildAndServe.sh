be511800b9e9948f00efc1fca85d7ab30c4e5f3c
2wenty1ne <sirdarkvic@gmail.com>
2025-02-10 01:39:40 +0000

    fixed buildAndServe.sh

--- checklist-server/buildAndServe.sh
+++ checklist-server/buildAndServe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd "/home/web/web-hosting/starwars-watchorder-checklist/checklist-client"
+cd /home/web/web-hosting/starwars-watchorder-checklist/checklist-client
 npm run build
 
-sudo rm -r "/home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/*"
+sudo rm -r /home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/*
 
-sudo mv "/home/web/web-hosting/starwars-watchorder-checklist/checklist-client/build/*" "/home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/"
+sudo mv /home/web/web-hosting/starwars-watchorder-checklist/checklist-client/build/* /home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/
