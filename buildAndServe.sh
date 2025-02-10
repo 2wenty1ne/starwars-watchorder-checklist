e3ac262169fdce1fc2dabdbeffda734f39041337
2wenty1ne <sirdarkvic@gmail.com>
2025-02-10 16:41:17 +0100

    small buildAndServer change

--- buildAndServe.sh
+++ buildAndServe.sh
@@ -6,3 +6,5 @@ npm run build
 sudo rm -r /home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/*
 
 sudo mv /home/web/web-hosting/starwars-watchorder-checklist/checklist-client/build/* /home/web/web-hosting/starwars-watchorder-checklist/checklist-server/public/
+
+pm2 reload checklist-backend
